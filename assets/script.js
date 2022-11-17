7107368cca9fa587615dc74b696a437c15958051
danielshang11 <danielshang11@gmail.com>
2022-11-16 21:43:40 -0800

    debugging

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,28 @@
 var questions = [
   {
-    title: 'Commonly used data types DO NOT include:',
-    answers: ['strings', 'booleans', 'alerts', 'numbers'],
-    correct: 'alerts',
+    title: 'which word contains letter a',
+    answers: ['apple', 'bed', 'cow', 'duck'],
+    correct: 'apple',
   },
   {
-    title: 'The condition in an if / else statement is enclosed within ____.',
-    answers: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-    correct: 'parentheses',
+    title: 'which word contains letter b',
+    answers: ['apple', 'bed', 'cow', 'duck'],
+    correct: 'bed',
   },
   {
-    title: 'Arrays in JavaScript can be used to store ____.',
-    answers: [
-      'numbers and strings',
-      'other arrays',
-      'booleans',
-      'all of the above',
-    ],
-    correct: 'all of the above',
+    title: 'which word contains letter o',
+    answers: ['apple', 'bed', 'cow', 'duck'],
+    correct: 'cow',
   },
   {
-    title:
-      'String values must be enclosed within ____ when being assigned to variables.',
-    answers: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-    correct: 'quotes',
+    title: 'which word contains letter u',
+    answers: ['apple', 'bed', 'cow', 'duck'],
+    correct: 'duck',
   },
   {
-    title:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
-    answers: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-    correct: 'console.log',
+    title: 'which word contains letter k',
+    answers: ['apple', 'bed', 'cow', 'duck'],
+    correct: 'duck',
   },
 ];
 
@@ -38,9 +31,12 @@ var questions = [
 
 var timerEl = document.getElementById("timecount")
 var queryEl = document.getElementById("query")
-var startBtn = document.getElementById('start-btn')
-
-var quizContainer = document.getElementById('qz-main')
+var startBtn = document.getElementById("start-btn")
+var creditEl = document.getElementById("credit")
+var answerEl = document.getElementById("answerselections")
+var startEl = document.getElementById("qz-start")
+var endEl = document.getElementById("qz-end")
+var creditEl = document.getElementById("credit") 
 
 
 
@@ -49,10 +45,18 @@ var time = 40;
 var timeVar;
 
 
+
+
+
+
+
 function quizBegin(){
-  var beginpageEl = document.getElementById('qz-start');
-  beginpageEl.setAttribute('class','hide');
-  queryEl.removeAttribute('class','hide');
+  
+  startEl.style.display = "none";
+  queryEl.style.display="block";
+  endEl.style.display = "none";
+  creditEl.style.display = "none";
+
   timeVar = setInterval(
     function timeCount() {
       time--;
@@ -63,32 +67,114 @@ function quizBegin(){
   setNextQuestion();
 }
 
-var currentQuestionIndex = 0
+let questionNowIndex = 0
 
 
 
 function setNextQuestion(){
-  var questionNow = questions[currentQuestionIndex];
-  var titleEl = document.getElementById('query-title');
+  var questionNow = questions[questionNowIndex];
+  var titleEl = document.getElementById("query-title");
   titleEl.textContent = questionNow.title;
-  queryEl.textContent = '';
+  answerEl.innerHTML = " ";
   for (var i=0; i < questionNow.answers.length ; i++ ){
     var answer = questionNow.answers[i];
-    var answerNode = document.createElement('button');
-    answerNode.setAttribute('class', 'answer');
-    answerNode.setAttribute('value', answer);
+    var answerNode = document.createElement(a-button);
+    answerNode.setAttribute("class", "answer");
+    answerNode.setAttribute("value", answer);
 
-    answerNode.textContent = i + 1 + '. ' + answer;
+    answerNode.textContent = i + 1 + ".  "+ answer;
 
-    queryEl.appendChild(answerNode);
+    answerEl.appendChild(answerNode);
 
   }
 
 }
 
+answerEl.onclick = questionAnswer
+
+function questionAnswer(event){
+  var choiceEl = event.target
+  if(!choiceEl.matches(".answer")){return};
+  if(choiceEl.valve !== questions[questionNowIndex].correct){
+    time = time -5;
+    if(time < 0){ time =0;}
+    timerEl.textContent = time;
+    creditEl.textContent = "oh no incorrect answer!";
+  } else {
+    creditEl.textContent = "Yes! you got it!";
+  }
+  creditEl.setAttribute("class", "credit");
+  setTimeout(function () {
+    creditEl.style.display="none";
+  }, 1000);
+
+}
+
+questionNowIndex++;
+
+
+  if (time <= 0 || questionNowIndex === questions.length) {
+    quizEnd();
+  } else {
+    quizBegin();
+  }
+
+function quizEnd() {
+ 
+  clearInterval(timerVar);
+
+  var endScreenEl = document.getElementById("qz-end");
+  endScreenEl.removeAttribute("class");
+
+
+  var finalScoreEl = document.getElementById("resultscore");
+  finalScoreEl.textContent = time;
+
+  
+  queryEl.style.display = "none";
+}
+
+
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+  if (initials !== '') {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = 'highscores.html';
+  }
+}
+
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
 
 
 
+
+
+initialsEl.onkeyup = checkForEnter;
 
 
 
